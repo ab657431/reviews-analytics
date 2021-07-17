af3dd9355a2f8761c736cba6e7f080b1515258cc
Bossn <yoyoyoyo894@gmail.com>
2021-07-17 22:08:04 +0800

    second

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -6,5 +6,11 @@ with open('reviews.txt' , 'r') as f:
 		count += 1
 		if count % 1000 ==0:
 			print(len(data))
-print(len(data))
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+
+sumlen = 0
+for d in data:
+	sumlen = sumlen +len(d)
+
+print('平均是', sumlen/len(data))
 
